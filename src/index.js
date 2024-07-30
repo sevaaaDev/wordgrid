f28c5c2cacd2b0757d000f2c50cf7c0df649bafa
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-07-30 10:38:17 +0700

    feat: prototype

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "./style.css";
+import { renderInit } from "./UI/render";
+import { Game } from "./Game/game";
+import { initBtn, initPlayAgainBtn, initSwipeSelect } from "./Input/input";
+import PubSub from "pubsub-js";
+
+let game = new Game();
+renderInit();
+initBtn();
+initPlayAgainBtn();
+initSwipeSelect();
+PubSub.subscribe("SendAnswer", game.checkWord.bind(game));
+PubSub.subscribe("StartGame", game.init.bind(game));
+PubSub.subscribe("ResetGame", game.reset.bind(game));
