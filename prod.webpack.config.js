22ef205c375aebdc8e532e48df55156231b820fe
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-08-04 11:44:47 +0700

    build: create two separate conf for webpack

---
+++ prod.webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: path.resolve(__dirname, "src"),
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
